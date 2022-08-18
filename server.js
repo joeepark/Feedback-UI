b8dcc29000420d543ca13642ca9bcad85b9314de
JosephPark <joeepark@gmail.com>
2022-08-18 14:37:07 -0700

    Configuring files to deploy on Heroku

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const data = require('./db.json')
-
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(data); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; // <== You can change the port
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
+  static: './build'
+});
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}));
 
+server.use(router);
 server.listen(port);
