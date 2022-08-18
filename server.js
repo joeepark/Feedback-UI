dc613e6dd19a71eb2813c7124ce9d8c468705cac
JosephPark <joeepark@gmail.com>
2022-08-18 14:44:25 -0700

    Configuring settings for Heroku Deployment

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-  static: './build'
-});
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-  '/api/*': '/$1',
-}));
-
 server.use(router);
+
 server.listen(port);
